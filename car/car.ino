6b2f5940f13dd4fc1da015b700f08811cd4f7862
Anthony A <anthonydc1972@outlook.com>
2025-02-03 23:26:05 -0600

    updates second semester

--- car/car.ino
+++ car/car.ino
@@ -4,42 +4,91 @@
 // Structure example to receive data
 // Must match the sender structure
 
-int ledPin = 2;
-int motorForwardPin = 12;
-int motorReversePin = 14;
+#define LED_PIN 2
+
+#define M1A 25
+#define M1B 26
+#define M2A 32
+#define M2B 33
+
+#define M1PWM 18
+#define M2PWM 19
+
+
+class Motor {
+  private:
+    int pinA;
+    int pinB;
+    int pinPWM;
+    bool reversed;
+  public:
+    void init(int pinA, int pinB, int pinPWM, bool reversed) {
+      this->pinA = pinA;
+      this->pinB = pinB;
+      this->pinPWM = pinPWM;
+      this->reversed = reversed;
+
+      pinMode(pinA, OUTPUT);
+      pinMode(pinB, OUTPUT);
+      pinMode(pinPWM, OUTPUT);
+      ledcAttachChannel(pinPWM, 30000, 8, 0);
+    }
+    void forward(float speed) {
+      int trueSpeed = 255 * speed;
+      Serial.println(trueSpeed);
+      ledcWrite(pinPWM, trueSpeed);
+      digitalWrite(pinA, HIGH);
+      digitalWrite(pinB, LOW);
+    }
+    void stop() {
+      // analogWrite(pinPWM, LOW);
+      digitalWrite(pinA, LOW);
+      digitalWrite(pinB, LOW);
+    }
+    void backward(float speed) {
+      int trueSpeed = 255 * speed;
+      ledcWrite(pinPWM, trueSpeed);
+      digitalWrite(pinA, LOW);
+      digitalWrite(pinB, HIGH);
+    }
+
+    void drive(float speed) {
+      if (speed > 0.1) {
+        forward(speed);
+      } else if (speed < -0.1) {
+        backward(speed);
+      } else {
+        stop();
+      }
+    }
+};
+
+enum DriveType {
+  ARCADE,
+  TANK,
+};
 
 typedef struct struct_message {
-  bool channel0;
-  double speed;
-  double direction;
+  DriveType mode;
+  float leftX;
+  float leftY;
+  float rightX;
+  float rightY;
+  int message;
 } struct_message;
 
 // Create a struct_message called recvData
 struct_message recvData;
+bool newData = false;
 
 // callback function that will be executed when data is received
-void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len) {
+void onDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len) {
   memcpy(&recvData, incomingData, sizeof(recvData));
-  Serial.print("ch0:");
-  Serial.print(recvData.channel0);
-  Serial.print(",spd:");
-  Serial.print(recvData.speed);
-  Serial.print(",dir:");
-  Serial.println(recvData.direction);
-  digitalWrite(ledPin, (bool)recvData.channel0);
-
-  if (recvData.speed > 0.2) {
-    digitalWrite(motorForwardPin, 1);
-    digitalWrite(motorReversePin, 0);
-  } else if (recvData.speed < -0.2) {
-    digitalWrite(motorReversePin, 1);
-    digitalWrite(motorForwardPin, 0);
-  } else {
-    digitalWrite(motorForwardPin, 0);
-    digitalWrite(motorReversePin, 0);
-  }
-
+  newData = true;
 }
+
+Motor left;
+Motor right;
 
 void setup() {
   // Initialize Serial Monitor
@@ -56,14 +105,67 @@ void setup() {
 
   // Once ESPNow is successfully Init, we will register for recv CB to
   // get recv packer info
-  esp_now_register_recv_cb(esp_now_recv_cb_t(OnDataRecv));
+  esp_now_register_recv_cb(esp_now_recv_cb_t(onDataRecv));
 
-  pinMode(ledPin, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
+  left.init(M1A, M1B, M1PWM, false);
+  right.init(M2A, M2B, M2PWM, true);
+
 }
 
+
 void loop() {
-  digitalWrite(ledPin, 1);
-  delay(200);
-  digitalWrite(ledPin, 0);
-  delay(400);
+
+  // left.drive(1);
+  // delay(1000);
+  // left.stop();
+  // delay(1000);
+  // left.drive(-1);
+  // delay(1000);
+  // left.stop();
+  // delay(1000);
+
+  if (newData) {
+    // do things
+
+    Serial.print(recvData.leftY);
+    Serial.print(", ");
+    Serial.println(recvData.rightY);
+    
+    float xSpeed, zRot, leftSpeed, rightSpeed, greaterInput, lesserInput, saturatedInput;
+    switch (recvData.mode) {
+      case ARCADE:
+        // handle arcade driving
+
+        xSpeed = recvData.leftY;
+        zRot   = recvData.rightX;
+
+        leftSpeed  = xSpeed - zRot;
+        rightSpeed = xSpeed + zRot;
+
+        greaterInput = max(abs(xSpeed), abs(zRot));
+        lesserInput  = min(abs(xSpeed), abs(zRot));
+        
+        if (greaterInput == 0.0) {
+          left.stop();
+          right.stop();
+        }
+
+        saturatedInput = (greaterInput + lesserInput) / greaterInput;
+        leftSpeed /= saturatedInput;
+        rightSpeed /= saturatedInput;
+
+        left.drive(leftSpeed);
+        right.drive(rightSpeed);
+
+        break;
+      case TANK: 
+        // handle tank drive
+        left.drive(recvData.leftY);
+        right.drive(recvData.rightY);
+    }
+
+    newData = false;
+  }
+  delay(100);
 }
--- remote/remote.ino
+++ remote/remote.ino
@@ -1,34 +1,85 @@
 #include <WiFi.h>
 #include <esp_now.h>
 
-#define BUTTON_PIN 12
-#define POT1_PIN 35 // gpio 35 (adc1_ch7)
-#define POT2_PIN 34 // gpio 34 (adc1_ch6)
+#define RIGHT_X_PIN 34   // gpio 34 (adc1_ch6)
+#define RIGHT_Y_PIN 32   // ?
+#define LEFT_X_PIN 0     //
+#define LEFT_Y_PIN 35    // gpio 35 (adc1_ch7)
 
+#define LEFT_BUTTON 25
+
+#define PWR_LED 22
+#define COM_LED 23
+
+#define sgn(x) ((x) < 0 ? -1 : ((x) > 0 ? 1 : 0))
+
+class SlewRateLimiter {
+  float lastValue;
+  float allowedDeltaPositive;
+  float allowedDeltaNegative;
+
+  public:
+   void init(float allowedDeltaPositive, float allowedDeltaNegative) {
+      this->allowedDeltaPositive = allowedDeltaPositive;
+      this->allowedDeltaNegative = allowedDeltaNegative;
+      lastValue = NULL;
+    } 
+    float calculate(float nextValue) {
+      float delta = nextValue -lastValue;
+      if (lastValue == NULL) {
+        lastValue = nextValue;
+      }
+      else if (delta > allowedDeltaPositive) {
+        lastValue += allowedDeltaPositive;
+      }
+      else if (delta < allowedDeltaNegative) {
+        lastValue -= allowedDeltaNegative;
+      }
+      else {
+        lastValue = nextValue;
+      }
+      return lastValue;
+    }
+};
+
+
+enum DriveType {
+  ARCADE,
+  TANK,
+};
 uint8_t broadcastAddress[] = {0xcc, 0xdb, 0xa7, 0x99, 0x60, 0x40};
 
-// Structure example to send data
-// Must match the receiver structure
 typedef struct struct_message {
-  bool channel0;
-  double speed;
-  double direction;
+  DriveType mode;
+  float leftX;
+  float leftY;
+  float rightX;
+  float rightY;
+  int message;
 } struct_message;
 
+SlewRateLimiter speedlim;
+SlewRateLimiter dirlim;
 struct_message sendData;
-
+bool comm_est = false;
+DriveType mode = ARCADE;
 esp_now_peer_info_t peerInfo;
+bool lastModeState = false;
+
+
 
 // callback when data is sent
-void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
-  // Serial.print("Last packet status:\t");
-  // Serial.println(status == ESP_NOW_SEND_SUCCESS ? "delivered ok" : "failed delivery");
+void onDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  if (status != ESP_NOW_SEND_SUCCESS) {
+    comm_est = false;
+  } else {
+    comm_est = true;
+  }
 }
- 
+
+
 void setup() {
   Serial.begin(115200);
- 
-  // Set device as a Wi-Fi Station
   WiFi.mode(WIFI_STA);
 
   // Init ESP-NOW
@@ -36,38 +87,80 @@ void setup() {
     Serial.println("Error initializing ESP-NOW");
     return;
   }
+  esp_now_register_send_cb(onDataSent);
 
-  // Once ESPNow is successfully Init, we will register for Send CB to
-  // get the status of transmitted packet
-  esp_now_register_send_cb(OnDataSent);
-  
   // Register peer
   memcpy(peerInfo.peer_addr, broadcastAddress, 6);
-  peerInfo.channel = 0;  
+  peerInfo.channel = 0;
   peerInfo.encrypt = false;
-  
-  // Add peer        
-  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+
+  // Add peer
+  if (esp_now_add_peer(&peerInfo) != ESP_OK) {
     Serial.println("Failed to add peer");
     return;
   }
 
-  pinMode(BUTTON_PIN, INPUT);
+  // set pin types
+  pinMode(LEFT_BUTTON, INPUT_PULLUP);
+  pinMode(PWR_LED, OUTPUT);
+  pinMode(COM_LED, OUTPUT);
+
+  pinMode(RIGHT_X_PIN, INPUT);
+  pinMode(RIGHT_Y_PIN, INPUT);
+  pinMode(LEFT_Y_PIN, INPUT);
+
+  // set pin attenuation so we don't blow the adc
+  analogSetPinAttenuation(RIGHT_X_PIN, ADC_ATTENDB_MAX);
+  analogSetPinAttenuation(LEFT_Y_PIN, ADC_ATTENDB_MAX);
+  analogSetPinAttenuation(RIGHT_Y_PIN, ADC_ATTENDB_MAX);
+
+
+  // SlewRateLimiters
+  speedlim.init(0.05, 0.05);
+  dirlim.init(0.05, 0.05);
 }
- 
+
 void loop() {
   // Set values to send
-  sendData.channel0 =   digitalRead(BUTTON_PIN);
-  sendData.speed =      analogRead(POT1_PIN) / 4095.0;
-  sendData.direction = (analogRead(POT2_PIN) / 4095.0)*2 - 1;
-  Serial.print("Channel_0:");
-  Serial.print(sendData.channel0);
+  bool modeButtonState = !digitalRead(LEFT_BUTTON);
+  float leftY  = (analogRead(LEFT_Y_PIN)  / 4095.0) * 2 - 1;    // tank left; ARCADE speed
+  float leftX  = 0;                                             // unused
+  float rightX = (analogRead(RIGHT_X_PIN) / 4095.0) * 2 - 1;    // ARCADE direction
+  float rightY = (analogRead(RIGHT_Y_PIN) / 4095.0) * 2 - 1;    // tank right
 
-  Serial.print(",Speed:");
-  Serial.print(sendData.speed); 
+  if (modeButtonState != lastModeState && modeButtonState == HIGH) {
+    mode = mode == TANK ? ARCADE : TANK;
+  }
+  lastModeState = modeButtonState;
 
-  Serial.print(",Direction:");
-  Serial.println(sendData.direction);
+  // deadzones
+  float deadzone = 0.2;
+  if (abs(leftY)  < deadzone) leftY  = 0;
+  if (abs(leftX)  < deadzone) leftX  = 0;
+  if (abs(rightY) < deadzone) rightY = 0;
+  if (abs(rightX) < deadzone) rightX = 0;
+
+  sendData.mode = mode;
+  sendData.leftY = leftY*leftY    * -sgn(leftY);
+  sendData.leftX = leftX*leftX    *  sgn(leftX);
+  sendData.rightX = rightX*rightX *  sgn(rightX);
+  sendData.rightY = rightY*rightY * -sgn(rightY);
+
+  Serial.print(",ModeState:");
+  Serial.print(sendData.mode); 
+  Serial.print(",LX:");
+  Serial.print(sendData.leftX); 
+  Serial.print(",LY:");
+  Serial.print(sendData.leftY); 
+  Serial.print(",RX:");
+  Serial.print(sendData.rightX); 
+  Serial.print(",RY:");   
+  Serial.println(sendData.rightY); 
+
+  if (comm_est) {digitalWrite(COM_LED, HIGH);}
+  else {digitalWrite(COM_LED, LOW);}
+  if (!sendData.mode) {digitalWrite(PWR_LED, HIGH);}
+  else {digitalWrite(PWR_LED, LOW);}
   
   esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &sendData, sizeof(sendData));
    
